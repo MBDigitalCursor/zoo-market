c4e0085ff16a4086537b1737983fbc61f347a944
ivenckunas <ignas.venckunas@gmail.com>
2023-02-13 11:39:33 +0200

    headings p m reset

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./nav.css";
 
 function Nav() {
-	return <div>Nav</div>;
+	return (
+		<div className="nav-container">
+			<div className="nav-links">
+				<a href="/">Produktai</a>
+				<a href="/">Kontaktai</a>
+			</div>
+			<img
+				src={require("../../assets/img/zoo-market-logo.png")}
+				alt="zoo market logo"
+			/>
+			<div className="nav-lang">
+				<a href="/">LT</a>
+				<a href="/">EN</a>
+			</div>
+		</div>
+	);
 }
 
 export default Nav;
