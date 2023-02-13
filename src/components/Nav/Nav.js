f335821d617a2fcb344fafc851dbcb411f96f3e9
Ignas V <97701289+ivenckunas@users.noreply.github.com>
2023-02-13 11:17:17 +0200

    Add files via upload

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./nav.css";
 
 function Nav() {
-	return <div>Nav</div>;
+	return (
+		<div className="nav-container">
+			<h1>test</h1>
+			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, odio nulla! Quos, nihil dicta magni excepturi culpa assumenda perspiciatis rem.</p>
+		</div>
+	);
 }
 
 export default Nav;
