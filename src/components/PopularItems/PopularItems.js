b15c0a6b17e90de644e33fb9cacd94f8dffc9997
Arturas <mixmln14@gmail.com>
2023-02-13 11:39:58 +0200

    pop items

--- src/components/PopularItems/PopularItems.js
+++ src/components/PopularItems/PopularItems.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./popularItems.css";
+import { Box, Stack } from "@mui/material";
 
 function PopularItems() {
-	return <div>PopularItems</div>;
+	return (
+		<Stack
+			className='pop-items-container'
+			direction='row'
+			justifyContent='space-between'
+			alignItems='flex-start'
+		>
+			<Box>
+				<h3 className='pop-items-title'>Populiarios prekės</h3>
+				<Box></Box>
+			</Box>
+			<Box>
+				<h3 className='pop-items-title'>Naujienos ir pasiūlymai</h3>
+			</Box>
+		</Stack>
+	);
 }
 
 export default PopularItems;
