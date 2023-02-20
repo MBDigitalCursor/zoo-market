50306f909939f0db3f9f9712a2de6f240f0b7914
Arturas <mixmln14@gmail.com>
2023-02-20 13:59:39 +0200

    form updated

--- src/components/Contacts/ContactForm.js
+++ src/components/Contacts/ContactForm.js
@@ -6,19 +6,18 @@ function ContactForm() {
 	const { pageLanguage } = useSelector((state) => state.generalSlice);
 
 	return (
-		<div className="contacts-form">
-			<form action="">
-				<label htmlFor="">{pageLanguage === "LT" ? "El. pašto adresas" : "Email address"}</label>
-				<input type="text" />
-				<label htmlFor="">{pageLanguage === "LT" ? "Jūsų žinutė" : "Your message"}</label>
+		<div className='contacts-form'>
+			<form action=''>
+				<label htmlFor=''>{pageLanguage === "LT" ? "El. pašto adresas" : "Email address"}</label>
+				<input type='text' />
+				<label htmlFor=''>{pageLanguage === "LT" ? "Jūsų žinutė" : "Your message"}</label>
 				<textarea
-					name=""
-					id=""
-					cols="30"
-					rows="4"
+					name=''
+					id=''
+					cols='30'
+					rows='4'
 				></textarea>
 			</form>
-			<button className="form-btn">{pageLanguage === "LT" ? "Išsiųsti" : "Send"}</button>
 		</div>
 	);
 }
