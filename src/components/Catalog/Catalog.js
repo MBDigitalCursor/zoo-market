a68aa80ee38fdac2aeecf76cbc4e623f6334da55
ivenckunas <ignas.venckunas@gmail.com>
2023-02-20 19:11:53 +0200

    images updated

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./catalog.css";
-import dog from "../../assets/icons/dog2.png";
-import cat from "../../assets/icons/cat2.png";
+import dog from "../../assets/icons/dog 2.svg";
+import cat from "../../assets/icons/cat 2.svg";
 import bird from "../../assets/icons/bird2.png";
 import hamster from "../../assets/icons/hamster2.png";
 import fish from "../../assets/icons/fish2.png";
@@ -59,14 +59,14 @@ function Catalog() {
 
 	return (
 		<div
-			id='catalog'
-			className='container'
+			id="catalog"
+			className="container"
 		>
-			<h3 className='catalog-title'>{pageLanguage === "LT" ? "Siūlomi produktai" : "Categories"}</h3>
-			<h3 className='catalog-mobile-title'>{pageLanguage === "LT" ? "Populiarios prekės" : "Popular products"}</h3>
+			<h3 className="catalog-title">{pageLanguage === "LT" ? "Siūlomi produktai" : "Categories"}</h3>
+			<h3 className="catalog-mobile-title">{pageLanguage === "LT" ? "Populiarios prekės" : "Popular products"}</h3>
 
-			<div className=' catalog-container'>
-				<div className='categories-left-side'>
+			<div className=" catalog-container">
+				<div className="categories-left-side">
 					{categories.map((category, i) => (
 						<CategoryItem
 							category={category}
@@ -74,8 +74,8 @@ function Catalog() {
 						/>
 					))}
 				</div>
-				<div className='catalog-right-side'>
-					<div className='right-side-photo'>
+				<div className="catalog-right-side">
+					<div className="right-side-photo">
 						<div
 							style={{
 								position: "relative",
@@ -83,25 +83,25 @@ function Catalog() {
 						>
 							<img
 								src={require("../../assets/img/dog-photo.png")}
-								alt='dog with jumper'
+								alt="dog with jumper"
 							/>
-							<p className='photo-credit'>Photo by Karsten Winegeart on Unsplash</p>
+							<p className="photo-credit">Photo by Karsten Winegeart on Unsplash</p>
 						</div>
 
 						{pageLanguage === "LT" ? (
-							<h4 className='photo-header'>
+							<h4 className="photo-header">
 								PREKIAUJAME <span>SVERIAMU</span> POPULIARIŲ <br /> GAMINTOJŲ SAUSU ĖDALU
 							</h4>
 						) : (
-							<h4 className='photo-header'>
+							<h4 className="photo-header">
 								WE SELL DRY FOOD <span>BY THE KILO</span> FROM <br /> POPULAR MANUFACTURERS
 							</h4>
 						)}
 					</div>
-					<div className='left-side-desc'>
+					<div className="left-side-desc">
 						<img
 							src={info}
-							alt='animals products description'
+							alt="animals products description"
 						/>
 					</div>
 				</div>
--- src/components/Catalog/MobileProducts/MobileProducts.js
+++ src/components/Catalog/MobileProducts/MobileProducts.js
@@ -4,8 +4,8 @@ import dogsCannedFoodArr from "../../../helpers/dogsCannedFood.js";
 import catsDryFoodArr from "../../../helpers/catsDryFood.js";
 import catsCannedFoodArr from "../../../helpers/catsCannedFood.js";
 import ProdsPagination from "./ProdsPagination.js";
-import dogIcon from "../../../assets/icons/dog2.png";
-import catIcon from "../../../assets/icons/cat2.png";
+import dogIcon from "../../../assets/icons/dog 2.svg";
+import catIcon from "../../../assets/icons/cat 2.svg";
 import { useSelector } from "react-redux";
 
 function MobileProducts() {
@@ -32,42 +32,42 @@ function MobileProducts() {
 	];
 
 	return (
-		<div className='products-with-pagination'>
-			<div className='products-with-pagination-logo'>
+		<div className="products-with-pagination">
+			<div className="products-with-pagination-logo">
 				<img
 					style={{
 						width: "70px",
 						height: "70px",
 					}}
 					src={dogIcon}
-					alt=''
+					alt=""
 				/>
 			</div>
 			{dogsCategoryTrees.map((cat, i) => (
 				<div
 					id={`${cat.products === dogsDryFoodArr ? "dogsDryFood" : "dogsCannedFood"}`}
 					key={i}
-					className='tree-products'
+					className="tree-products"
 				>
 					<h4>{cat.title}</h4>
 					<ProdsPagination products={cat.products} />
 				</div>
 			))}
-			<div className='products-with-pagination-logo'>
+			<div className="products-with-pagination-logo">
 				<img
 					style={{
 						width: "50px",
 						height: "50px",
 					}}
 					src={catIcon}
-					alt=''
+					alt=""
 				/>
 			</div>
 			{catsCategoryTrees.map((cat, i) => (
 				<div
 					id={`${cat.products === catsDryFoodArr ? "catsDryFood" : "catsCannedFood"}`}
 					key={i}
-					className='tree-products'
+					className="tree-products"
 				>
 					<h4>{cat.title}</h4>
 					<ProdsPagination products={cat.products} />
