571fa9e9643222a7fd150a178a98ad4725c7e0be
tonispagonis <anton.polujanov@gmail.com>
2023-02-23 10:50:45 +0200

    dog sausas EN done

---
+++ src/helpers/EN/catsCannedFood.js
@@ -0,0 +1,185 @@
+import img1 from "../../assets/img/catsCannedFood/cat konservai 1.png";
+import img2 from "../../assets/img/catsCannedFood/cat konservai 2.png";
+import img3 from "../../assets/img/catsCannedFood/cat konservai 3.png";
+import img4 from "../../assets/img/catsCannedFood/cat konservai 4.png";
+import img5 from "../../assets/img/catsCannedFood/cat konservai 5.png";
+import img6 from "../../assets/img/catsCannedFood/cat konservai 6.png";
+import img7 from "../../assets/img/catsCannedFood/cat konservai 7.png";
+import img8 from "../../assets/img/catsCannedFood/cat konservai 8.png";
+import img9 from "../../assets/img/catsCannedFood/cat konservai 9.png";
+import img10 from "../../assets/img/catsCannedFood/cat konservai 10.png";
+
+const catsCannedFoodArr = [
+	{
+		img: img1,
+		desc: "Hill's Science Plan Adult Medium Lamb & Rice Dog Food",
+		price: "$TBA",
+		fullDesc: {
+			analysis: ["Protein 21.2%", "Fat content 14.2%", "Crude fiber 1.7%", "Omega-6 fatty acids 3.2%", "Crude ash 4.7% ", "Calcium 0.76%", "Phosphorus 0.64%", "Sodium 0.24%", "Potassium 0.65%", "Magnesium 0.11%"],
+			vitPerKg: ["Vitamin A 5,817 IU", "Vitamin D3438 IU", "Vitamin E650 mg", "Vitamin C70 mg", "Beta-carotene 1.5 mg"],
+			feedingGuide: ["Body Weight (kg) - Dry (g)", "2.5 - 60", "5 - 100", "7.5 - 135", "10 - 170", "20 - 285", "30 - 385", "40 - 480", "50 - 565", "60+ - 11 per kg"],
+			desc: {
+				firstPart: "Hill's Science Plan Adult dog food with chicken is specially formulated to fuel the energy needs of dogs during the prime of their life.",
+				secondPart: "Made with high quality, easy-to-digest ingredients. Omega-6s & Vitamin E for beautiful skin & coat. High-quality protein for lean muscles. Easy-to-digest ingredients and fibers for healthy digestion.",
+				thirdPart: "Recommended for Adult dogs 1 - 6 years of age.",
+				keyBenefits: "This dry dog food uses prebiotic fiber to feed your small dog's microbiome for optimal digestive health. Helps to maintain beautiful skin and a healthy coat by providing a precise balance of Omega-6 & Vitamin E. Supports lean muscle growth and maintenance in your adult dog with a premium source of high quality protein. Packed with natural fibers to promote healthy digestion for your fully grown dog. Made with high quality ingredients. Veterinarian Recommended.",
+				ingredients: "Maize, wheat, lamb meal, soybean meal, animal fat, maize gluten meal, brewers' rice, vegetable oil, flaxseed, minerals.",
+			},
+		},
+	},
+	{
+		img: img2,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img3,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img4,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img5,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img6,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img7,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img8,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img9,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img10,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+];
+
+export default catsCannedFoodArr;
---
+++ src/helpers/EN/dogsCannedFood.js
@@ -0,0 +1,185 @@
+import img1 from "../../assets/img/dogsCannedFood/dog konservai 1.png";
+import img2 from "../../assets/img/dogsCannedFood/dog konservai 2.png";
+import img3 from "../../assets/img/dogsCannedFood/dog konservai 3.png";
+import img4 from "../../assets/img/dogsCannedFood/dog konservai 4.png";
+import img5 from "../../assets/img/dogsCannedFood/dog konservai 5.png";
+import img6 from "../../assets/img/dogsCannedFood/dog konservai 6.png";
+import img7 from "../../assets/img/dogsCannedFood/dog konservai 7.png";
+import img8 from "../../assets/img/dogsCannedFood/dog konservai 8.png";
+import img9 from "../../assets/img/dogsCannedFood/dog konservai 9.png";
+import img10 from "../../assets/img/dogsCannedFood/dog konservai 10.png";
+
+const dogsCannedFoodArr = [
+	{
+		img: img1,
+		desc: "Calibra Dog Life Sensitive Rabbit 400 g",
+		price: "€3.10",
+		fullDesc: {
+			analysis: ["Crude protein 8.5%", "Crude fat 3%", "Crude fiber 1%", "Crude ash 2.5% ", "Omega-3 0.15%", "Omega-6 1%", "Moisture 78%", "Calcium 0.3%", "Phosphorus 0.25%", "Sodium 0.3%"],
+			vitPerKg: ["Vitamin A (3a672a) 800 IU", "Vitamin B1 (3a821) 1 mg", "Vitamin D3 (3a671) 200 IU", "Vitamin E (3a700) 120 mg", "Biotin (3a880) 0.4 mg", "Zinc (3b606) 10 mg", "Manganese (3b504) 2.4 mg", "Iodine (3b201) 0.6 mg", "Copper (3b406) 3.2 mg", "Iron (3b106) 8 mg"],
+			feedingGuide: ["Body Weight (kg) - Food (g)", "5 - 305", "10 - 515", "15 - 700", "20 - 870", "25 - 1025", "30 - 1175", "40 - 1460", "50 - 1730", "60 - 1980", "70 - 2225", "80 - 2460"],
+			desc: {
+				firstPart: "Calibra Dog Life Sensitive Rabbit - This canned food recipe with rabbit is a complete food designed for adult dogs, and is also suitable for sensitive digestion.",
+				secondPart: "The hypoallergenic recipe, based on a single protein, is formulated with a limited number of ingredients.",
+				thirdPart: "The recipe composition is grain- and gluten-free, which has an effect on its excellent digestibility. It contains only 100% defined ingredients of the highest quality.",
+				keyBenefits: "The recipe contains salmon oil, a source of Omega-3 unsaturated fatty acids (EPA, DHA), necessary for brain development and function, and to support the physiological functions of the skin and coat and the quality thereof. Omega-3 also has anti-inflammatory properties. Schizochytrium limacinum (dried seaweed) - a source of the Omega-3 fatty acid DHA, which supports normal brain function.",
+				ingredients: "Rabbit (70 %), pea flour, lignocellulose, salmon oil (1 %), calcium carbonate, dried algae (0.2 %, Schizochytrium limacinum).",
+			},
+		},
+	},
+	{
+		img: img2,
+		desc: "Advance Veterinary Diets Gastroenteric Wet Pouch 150 g",
+		price: "$TBA",
+		fullDesc: {
+			analysis: ["Crude protein 8%", "Crude fat 2.5%", "Crude fiber 0.3%", "Crude ash 2.3%", "Moisture 79%", "Sodium 0.1%", "Potassium 0.2%"],
+			vitPerKg: ["Vitamin D3 200 IU", "Vitamin E 20 mg", "Iodine 0.75 mg", "Copper 1.75 mg", "Manganese 1.4 mg", "Zinc 25 mg"],
+			feedingGuide: ["Body Weight (kg) - Pouches (per day)", "2-5 - 1.5-2.5", "6-10 - 3-4.5", "11-15 - 4.5-6", "16-20 - 6-7", "21-30 - 7.5-9.5", "31-40 - 10-12", "41-50 - 12.5-14.5"],
+			desc: {
+				firstPart: "Complete and balanced dietetic product for puppies over 14 weeks old and adult dogs, prepared with easily digestible ingredients, a low fibre content, and increased sodium and potassium content to help relieve certain gastrointestinal problems and aid with cases of exocrine pancreatic insufficiency.",
+				secondPart: "Its easy-to-digest formula facilitates the absorption of essential nutrients.",
+				thirdPart: "Veterinary diets canine gastroenteric is a clinically tested food with proven results.",
+				keyBenefits: "Soursce of chicken protein to help digestion. Flavourful food to improve appetite. Easily digestible rice contributes to generating a smaller volume of faeces. Prebiotics (FOS & MOS) promote the development of healthy intestinal flora.",
+				ingredients: "Meat and animal derivatives (chicken 43%), cereals (rice 8%), minerals, oils and fats, derivatives of vegetable origin (fructo-oligosaccharides 0.1%), mannan-oligosaccharides 0.1%.",
+			},
+		},
+	},
+	{
+		img: img3,
+		desc: "Nature's Variety Adult Madium/Maxi Beef Original Paté 300 g",
+		price: "€3.70",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "Complete and balanced pet food for adult dogs, no grain. With high-quality beef as the main ingredient, paired with fruits and vegetables. Steam cooked in its own juices and at optimal temperature for better nutrient conservation.",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "Complete & balanced, high quality protein, no artificial colours, flavours or preservatives, made with natural ingredients, steam cooked, trusted suppliers.",
+				ingredients: "Beef (66%), beef broth (28.9%), spinach (2%), pumpkin (2%), minerals, salmon oil.",
+			},
+		},
+	},
+	{
+		img: img4,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img5,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img6,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img7,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img8,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img9,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+	{
+		img: img10,
+		desc: "Royal Canin Pussy Juice Extra Wet, 1 kg",
+		price: "$14.99",
+		fullDesc: {
+			analysis: ["Protein 21.2%"],
+			vitPerKg: [""],
+			feedingGuide: [""],
+			desc: {
+				firstPart: "",
+				secondPart: "",
+				thirdPart: "",
+				keyBenefits: "",
+				ingredients: "",
+			},
+		},
+	},
+];
+
+export default dogsCannedFoodArr;
